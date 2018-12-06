322a257f1cfdbf2968d912d9d873732d39280726
mimbele <mrym.ndr@gmail.com>
2018-12-06 12:27:47 +0330

    newGeneration.m added

---
+++ newGeneration.m
@@ -0,0 +1,36 @@
+function [AA,BB]=newGeneration(A,B,g)
+    %generates new generation out of the old one(A)
+    %A is a 20x4 array A(x1,x2,x3,x4,generation)
+    %B is a 20 size array B(i) = (fn (A(i)s), generation)
+    %g is num of generation. new generation is g+1
+    AA = zeros(20,4);
+    BB = zeros(20,1);
+    maxB = 0;
+    maxi = 0;
+    for i=1:20
+        if B(i)>maxB
+            maxB = B(i);
+            maxi = i;
+        end
+    end
+    
+    BB(1)= maxB;
+    AA(1,1:4) = A(maxi,1:4);
+    
+    for i = 1:20
+        t(i) = round(B(i)/sum(B)*20); % number of each gene in new generation
+    end
+    counter=1;
+    for j=2:20
+        if t(j) ~= 0 && counter<20
+            for i=1:t(j)
+                counter=counter+1;
+                AA(counter,1:4)=A(j,1:4);
+            end
+        end
+    end
+    
+    for i=2:20
+        BB(i)= fn(A(i,1),A(i,2),A(i,3),A(i,4));
+    end
+end
